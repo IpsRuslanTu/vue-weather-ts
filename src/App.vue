595567ffea9c412f299215336fc9728ff3671136
Ruslan <r1tu@yandex.ru>
2023-08-26 13:34:23 -0700

    add antd package

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,21 @@
 <template>
-    Test
+    <div class="app">
+        <a-spin :spinning="true">
+            Test
+        </a-spin>
+    </div>
 </template>
+
+<style lang="scss">
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+.app {
+    margin: 0 auto;
+    width: 200px;
+    border: 1px solid grey;
+}
+</style>
